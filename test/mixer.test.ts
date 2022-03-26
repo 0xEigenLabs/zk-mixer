86239b8448882a2bca09eccca987141a69882a6c
BC-A <oneyuanju@gmail.com>
2022-03-26 06:20:06 -0700

    fix test file

--- test/mixer.test.ts
+++ test/mixer.test.ts
@@ -1,4 +1,4 @@
-const { waffle, ethers}  = require("hardhat");
+const { waffle, ethers } = require("hardhat");
 import { ContractFactory, BigNumberish } from "ethers";
 import { assert, expect } from "chai";
 const BN = require("bn.js");
@@ -6,9 +6,11 @@ const fs = require("fs");
 const path = require("path");
 const F1Field = require("ffjavascript").F1Field;
 const snarkjs = require("snarkjs");
+import { utils } from "ffjavascript";
+const { stringifyBigInts, unstringifyBigInts } = utils;
 import * as MIMCMerkle from "../lib/MiMCMerkle";
 const {
-  randomBytes
+    randomBytes
 } = require('crypto');
 
 const provider = waffle.provider
@@ -24,7 +26,7 @@ interface Proof {
 }
 
 function Bits2Num(n, in1) {
-    var lc1=0;
+    var lc1 = 0;
 
     var e2 = 1;
     for (var i = 0; i < n; i++) {
@@ -47,17 +49,17 @@ function parseProof(proof: any): Proof {
 
 // deposit
 // 0,1,2,3
-async function deposit(mixerInstance, signer, cmt){
-  var tx = await mixerInstance.deposit(cmt, {
-    from: signer,
-    value: Amount
-  });
-  await tx.wait()
-  console.log("Deposit done")
+async function deposit(mixerInstance, signer, cmt) {
+    var tx = await mixerInstance.deposit(cmt, {
+        from: signer,
+        value: Amount
+    });
+    await tx.wait()
+    console.log("Deposit done")
 }
 
 async function getMimc(mixerInstance, input, sk) {
-    let abi = [ "event TestMimc(uint256)" ]
+    let abi = ["event TestMimc(uint256)"]
     var iface = new ethers.utils.Interface(abi)
     let tx = await mixerInstance.getMimc(input, sk)
     let receipt = await tx.wait()
@@ -67,33 +69,33 @@ async function getMimc(mixerInstance, input, sk) {
 
 // getMerkleProof
 async function getMerkleProof(mixerInstance, leaf_index) {
-  let res = []
-  let addressBits = []
-  let tx = await mixerInstance.getMerkleProof(leaf_index);
-  let receipt = await tx.wait()
+    let res = []
+    let addressBits = []
+    let tx = await mixerInstance.getMerkleProof(leaf_index);
+    let receipt = await tx.wait()
 
-  let abi = [ "event MerkleProof(uint256[8] , uint256[8] )" ]
-  var iface = new ethers.utils.Interface(abi);
-  let logs = iface.parseLog(receipt.events[0]); 
-  let proof = logs.args[0]
-  let proof2 = logs.args[1]
+    let abi = ["event MerkleProof(uint256[8] , uint256[8] )"]
+    var iface = new ethers.utils.Interface(abi);
+    let logs = iface.parseLog(receipt.events[0]);
+    let proof = logs.args[0]
+    let proof2 = logs.args[1]
 
-  for (let i=0 ;i< proof.length;i++){
-    let t =  proof[i];
-    res.push(t.toString())
-  }
+    for (let i = 0; i < proof.length; i++) {
+        let t = proof[i];
+        res.push(t.toString())
+    }
 
-  for (let i=0 ;i< proof2.length;i++){
-    let t =  proof2[i];
-    addressBits.push(t.toString())
-  }
-  return [res, addressBits];
+    for (let i = 0; i < proof2.length; i++) {
+        let t = proof2[i];
+        addressBits.push(t.toString())
+    }
+    return [res, addressBits];
 }
 
 async function getRoot(mixerInstance) {
-  let root =  await mixerInstance.getRoot();
-  console.log("root:", root.toString())
-  return root.toString()
+    let root = await mixerInstance.getRoot();
+    console.log("root:", root.toString())
+    return root.toString()
 }
 
 /*
@@ -113,7 +115,7 @@ async function getRootEx(mixerInstance, leaf, cmtIdx) {
 
 const getUniqueLeaf = (mimc, leaf, depth) => {
     if (leaf == 0) {
-        for (let i = 0; i < depth; i ++) {
+        for (let i = 0; i < depth; i++) {
             leaf = mimc.hash(leaf, leaf);
         }
     }
@@ -121,7 +123,7 @@ const getUniqueLeaf = (mimc, leaf, depth) => {
 }
 
 
-const runTest = async(signer, contract, mimcJS, path2RootPos) => {
+const runTest = async (signer, contract, mimcJS, path2RootPos) => {
     // secret
     const secret = "011"
     const cmtIdx = Bits2Num(8, path2RootPos)
@@ -148,7 +150,7 @@ const runTest = async(signer, contract, mimcJS, path2RootPos) => {
      */
 
     let root = leaf;
-    for (var i = 0; i < 8; i ++) {
+    for (var i = 0; i < 8; i++) {
         if (path2RootPos2[i] == 1) {
             root = mimcJS.hash(root, merklePath[i])
         } else {
@@ -171,6 +173,7 @@ const runTest = async(signer, contract, mimcJS, path2RootPos) => {
     let wasm = path.join(__dirname, "../circuit/mixer_js", "mixer.wasm");
     let zkey = path.join(__dirname, "../circuit/mixer_js", "circuit_final.zkey");
     let vkeypath = path.join(__dirname, "../circuit/mixer_js", "verification_key.json");
+    let proofPath = path.join(__dirname, "../circuit/mixer_js", "proof.json");
     const wc = require("../circuit/mixer_js/witness_calculator");
     const buffer = fs.readFileSync(wasm);
     const witnessCalculator = await wc(buffer);
@@ -179,10 +182,12 @@ const runTest = async(signer, contract, mimcJS, path2RootPos) => {
         input,
         0
     );
-
-    const { proof, publicSignals } = await snarkjs.groth16.prove(zkey, witnessBuffer);
-    const {a, b, c} = parseProof(proof);
-
+    //const { proof, publicSignals } = await snarkjs.plonk.prove(zkey, witnessBuffer);
+    const proofJson = unstringifyBigInts(JSON.parse(fs.readFileSync(proofPath, "utf8")));
+    const res = await snarkjs.plonk.exportSolidityCallData(proofJson, "");
+    let result = res.substring(0, res.length - 3);
+    console.log("result:", result)
+    //const {a, b, c} = parseProof(proof);
     console.log(await getRoot(contract));
     let inputTest = [
         //await getRoot(contract),
@@ -191,58 +196,58 @@ const runTest = async(signer, contract, mimcJS, path2RootPos) => {
     ]
     console.log("Withdraw 1111", inputTest)
     await contract.withdraw(
-        a, b, c,
+        result,
         inputTest
     )
-    }
+}
 
-    describe("Mixer test suite", () => {
-        let contract
-        let mimcJS
-        let mimcContract
-        let signer
-        before(async() => {
-            let signer = provider.getSigner(0)
-            mimcJS = await cls.buildMimc7();
-            let abi = cls.mimc7Contract.abi
-            let createCode = cls.mimc7Contract.createCode
-            let f = new ContractFactory(
-                abi, createCode(SEED, 91), signer
-            )
-            mimcContract = await f.deploy()
+describe("Mixer test suite", () => {
+    let contract
+    let mimcJS
+    let mimcContract
+    let signer
+    before(async () => {
+        let signer = provider.getSigner(0)
+        mimcJS = await cls.buildMimc7();
+        let abi = cls.mimc7Contract.abi
+        let createCode = cls.mimc7Contract.createCode
+        let f = new ContractFactory(
+            abi, createCode(SEED, 91), signer
+        )
+        mimcContract = await f.deploy()
 
-            console.log(mimcContract.address)
-            let F = await ethers.getContractFactory("Mixer");
-            contract = await F.deploy(mimcContract.address);
+        console.log(mimcContract.address)
+        let F = await ethers.getContractFactory("Mixer");
+        contract = await F.deploy(mimcContract.address);
 
-            await MIMCMerkle.init();
-        })
-
-        it ("Test MIMC", async() => {
-            const res = await mimcContract["MiMCpe7"](1,2);
-            const res2 = mimcJS.hash(1,2);
-            assert.equal(res.toString(), mimcJS.F.toString(res2));
-
-            let r = "17476463353520328933908815096303937517517835673952302892565831818490112348179";
-            console.log(mimcJS.F.toString(mimcJS.hash(r, r)))
-            console.log(mimcJS.F.toString(await mimcContract["MiMCpe7"](r, r)))
-
-            await getMimc(contract, r, r)
-        })
-
-        it("Test Mixer Withdraw", async() => {
-            // secret
-            let path2RootPos = [0, 0, 0, 0, 0, 0, 0, 0]
-            await runTest(signer, contract, mimcJS, path2RootPos)
-
-            path2RootPos = [1, 0, 0, 0, 0, 0, 0, 0]
-            await runTest(signer, contract, mimcJS, path2RootPos)
-            
-            path2RootPos = [0, 1, 0, 0, 0, 0, 0, 0]
-            await runTest(signer, contract, mimcJS, path2RootPos)
-
-            // should failed
-            //path2RootPos = [0, 0, 1, 0, 0, 0, 0, 0]
-            //await runTest(signer, contract, mimcJS, path2RootPos)
-        })
+        await MIMCMerkle.init();
     })
+
+    it("Test MIMC", async () => {
+        const res = await mimcContract["MiMCpe7"](1, 2);
+        const res2 = mimcJS.hash(1, 2);
+        assert.equal(res.toString(), mimcJS.F.toString(res2));
+
+        let r = "17476463353520328933908815096303937517517835673952302892565831818490112348179";
+        console.log(mimcJS.F.toString(mimcJS.hash(r, r)))
+        console.log(mimcJS.F.toString(await mimcContract["MiMCpe7"](r, r)))
+
+        await getMimc(contract, r, r)
+    })
+
+    it("Test Mixer Withdraw", async () => {
+        // secret
+        let path2RootPos = [0, 0, 0, 0, 0, 0, 0, 0]
+        await runTest(signer, contract, mimcJS, path2RootPos)
+
+        path2RootPos = [1, 0, 0, 0, 0, 0, 0, 0]
+        await runTest(signer, contract, mimcJS, path2RootPos)
+
+        path2RootPos = [0, 1, 0, 0, 0, 0, 0, 0]
+        await runTest(signer, contract, mimcJS, path2RootPos)
+
+        // should failed
+        //path2RootPos = [0, 0, 1, 0, 0, 0, 0, 0]
+        //await runTest(signer, contract, mimcJS, path2RootPos)
+    })
+})
